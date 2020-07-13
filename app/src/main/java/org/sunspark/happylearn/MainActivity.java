767826aa6c5d834051fad1e3dcfa6b6ac3d84475
reznik1024 <ron.reznikov@gmail.com>
2020-07-14 02:29:24 +0300

    project

--- app/src/main/java/org/sunspark/happylearn/MainActivity.java
+++ app/src/main/java/org/sunspark/happylearn/MainActivity.java
@@ -1,26 +1,45 @@
 package org.sunspark.happylearn;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     SharedPreferences sp;
     TextView tx;
     FirebaseAuth fAuth;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         fAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
        if(fAuth.getCurrentUser() ==null)updateUI();
         SharedPreferences sp = getApplicationContext().getSharedPreferences("NotLogedIn", Context.MODE_PRIVATE);
        tx  = (TextView) findViewById(R.id.text);
@@ -41,4 +60,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.Home){
+
+        }
+        if(item.getItemId() == R.id.Profile){
+
+        }
+        return true;
+    }
 }
